ea026b34023cd22044b81bcfa5e07c434eedb512
Mehdi <mah.fat@gmail.com>
2023-05-15 02:20:35 +0330

    Update rentcalc.py

--- rentcalc.py
+++ rentcalc.py
@@ -3,52 +3,56 @@ from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QLabel, QLineEdit, QPushButton, QGridLayout, QFrame
 
 
+
 class RentCalculator(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("Rent Calculator")
+        self.setWindowTitle("محاسبه ‌گر رهن و اجاره")
+        self.setLayoutDirection(Qt.RightToLeft)  # Set layout direction to right-to-left
 
         # Total property price
-        total_price_label = QLabel("Total Property Price:")
+        total_price_label = QLabel("قیمت ملک:")
+        total_price_label.setAlignment(Qt.AlignRight)
         self.total_price_input = QLineEdit()
 
         # Total security deposit
-        self.security_deposit_label = QLabel("Total Security Deposit:")
+        self.security_deposit_label = QLabel("رهن کامل (برابر با یک هشتم ارزش ملک):")
         self.security_deposit_output = QLabel()
 
         # Change Deposit label
-        change_deposit_label = QLabel("Change Deposit")
-        change_deposit_label.setStyleSheet("color: red")
+        change_deposit_label = QLabel("تغییر <b>ودیعه</b>")
+        total_price_label.setAlignment(Qt.AlignRight)
+        change_deposit_label.setStyleSheet("color: blue; font-weight: bold;")
 
         # Deposit input
-        deposit_label = QLabel("Deposit:")
+        deposit_label = QLabel("ودیعه:")
         self.deposit_input = QLineEdit()
 
         # Monthly rent input
-        monthly_rent_label = QLabel("Monthly Rent:")
+        monthly_rent_label = QLabel("اجاره ماهانه:")
         self.monthly_rent_input = QLineEdit()
 
         # Calculate rent button
-        calculate_rent_button = QPushButton("Calculate Rent")
+        calculate_rent_button = QPushButton("اجاره را محاسبه کن")
         calculate_rent_button.clicked.connect(self.calculate_rent)
 
         # Rent input
-        rent_label = QLabel("Monthly Rent:")
+        rent_label = QLabel("اجاره ماهانه:")
         self.rent_input = QLineEdit()
 
         # Calculate deposit button
-        calculate_deposit_button = QPushButton("Calculate Deposit")
+        calculate_deposit_button = QPushButton("ودیعه را محاسبه کن")
         calculate_deposit_button.clicked.connect(self.calculate_deposit)
 
         # Deposit output label
-        deposit_output_label = QLabel("Deposit:")
+        deposit_output_label = QLabel("ودیعه:")
         self.deposit_output = QLineEdit()
         self.deposit_output.setReadOnly(True)
 
-    
         # Change Rent label
-        change_rent_label = QLabel("Change Rent")
+        change_rent_label = QLabel("تغییر <b>اجاره</b>")
+        change_rent_label.setStyleSheet("color: blue; font-weight: bold;")
 
         # Create grid layout
         grid = QGridLayout()
@@ -79,6 +83,25 @@ class RentCalculator(QMainWindow):
 
         self.setGeometry(100, 100, 400, 300)
         self.show()
+
+        # Set right alignment for labels
+        total_price_label.setAlignment(Qt.AlignRight)
+        self.security_deposit_label.setAlignment(Qt.AlignRight)
+        change_deposit_label.setAlignment(Qt.AlignRight)
+        deposit_label.setAlignment(Qt.AlignRight)
+        monthly_rent_label.setAlignment(Qt.AlignRight)
+        rent_label.setAlignment(Qt.AlignRight)
+        deposit_output_label.setAlignment(Qt.AlignRight)
+        change_rent_label.setAlignment(Qt.AlignRight)
+
+        # Set right alignment for outputs
+        self.security_deposit_output.setAlignment(Qt.AlignRight)
+        self.deposit_output.setAlignment(Qt.AlignRight)
+        self.monthly_rent_input.setAlignment(Qt.AlignRight)
+        self.rent_input.setAlignment(Qt.AlignRight)
+        self.deposit_input.setAlignment(Qt.AlignRight)
+
+
 
     def calculate_rent(self):
         try:
